d9de1f556f6ea25a91805bf2fa5b49626da2d52a
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-02-27 19:37:08 +0200

    Ball logic and some game mechanics

--- brickbreakergame/falusvampen/game.js
+++ brickbreakergame/falusvampen/game.js
@@ -85,11 +85,11 @@ function movePaddleRight() {
 let ballReleased = false;
 
 // Initial position of the ball
-
 function holdball() {
   ball.style.left = paddle.offsetLeft + paddleWidth / 2 - 10 + "px";
   ball.style.top = paddle.offsetTop - 35 + "px";
 }
+// Running the function here makes the ball always start in the middle of the paddle
 holdball();
 
 // Start the game with spacebar
@@ -100,4 +100,109 @@ document.addEventListener("keydown", function (event) {
       moveBall();
     }
   }
+});
+
+let ballDirectionX = 1;
+let ballDirectionY = -1;
+let ballSpeed = 5;
+
+function moveBall() {
+  let ballX = ball.offsetLeft;
+  let ballY = ball.offsetTop;
+  if (ballReleased) {
+    requestAnimationFrame(function () {
+      // Ball hits the top
+      if (ballY < 0) {
+        ballDirectionY = 1;
+        sound("tap");
+      }
+      // Ball hits the bottom
+      if (ballY > gameScreen.offsetHeight - 20) {
+        livesCounter();
+        ballDirectionX = 1;
+        ballDirectionY = -1;
+        ballReleased = false;
+      }
+      // Ball hits the left
+      if (ballX < 0) {
+        ballDirectionX = 1;
+        sound("tap");
+      }
+      // Ball hits the right
+      if (ballX > gameScreenWidth - 20) {
+        ballDirectionX = -1;
+        sound("tap");
+      }
+      // Ball hits the paddle
+      if (
+        ballX + 20 > paddle.offsetLeft &&
+        ballX < paddle.offsetLeft + paddleWidth &&
+        ballY + 20 > paddle.offsetTop
+      ) {
+        // Play a sound from the music folder when the ball hits the paddle
+        ballDirectionY = -1;
+        sound("hit");
+      }
+
+      // Ball hits the brick
+      for (let i = 0; i < brick.length; i++) {
+        if (
+          ballX + 20 > brick[i].offsetLeft &&
+          ballX < brick[i].offsetLeft + brick[i].offsetWidth &&
+          ballY + 20 > brick[i].offsetTop &&
+          ballY < brick[i].offsetTop + brick[i].offsetHeight
+        ) {
+          ballDirectionY = 1;
+          brick[i].style.display = "none";
+          ballReleased = false;
+          scoreCounter();
+        }
+      }
+      // This makes the ball move
+      ballX += ballDirectionX * ballSpeed;
+      ballY += ballDirectionY * ballSpeed;
+      ball.style.left = ballX + "px";
+      ball.style.top = ballY + "px";
+      moveBall();
+    });
+  } else {
+    holdball();
+  }
+}
+
+// Game mechanics
+
+// Lives counter
+function livesCounter() {
+  // the lives counter is a string with a number in it "Lives: 3" so we need to split it and get the number
+  let currentLives = parseInt(lives.innerHTML.split(" ")[1]);
+  currentLives--;
+  lives.innerHTML = "Lives: " + currentLives;
+  if (currentLives === 0) {
+    alert("Game over");
+    document.location.reload();
+  }
+}
+
+// Score counter
+function scoreCounter() {
+  let currentScore = parseInt(score.innerHTML.split(" ")[1]);
+  currentScore++;
+  score.innerHTML = "Score: " + currentScore;
+  if (currentScore === brick.length) {
+    alert("You won!");
+    document.location.reload();
+  }
+}
+
+// Sound effects and music
+function sound(src) {
+  let sound = new Audio("music/" + src + ".wav");
+  sound.play();
+}
+
+// make sure that the sound effects and music are loaded before the game starts
+window.addEventListener("load", function () {
+  sound("tap");
+  sound("hit");
 });
