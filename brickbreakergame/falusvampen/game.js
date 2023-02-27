52d0ac271e86d3488552bc2a8b1b9558389e76b7
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2023-02-27 16:33:30 +0200

    added smooth paddle movement and holdBall function

--- brickbreakergame/falusvampen/game.js
+++ brickbreakergame/falusvampen/game.js
@@ -7,18 +7,97 @@ const paddle = document.getElementById("paddle");
 const ball = document.getElementById("ball");
 const brick = document.getElementsByClassName("brick");
 
-// Set up event listeners for keyboard input
-document.addEventListener("keydown", (event) => {
+// Paddle movement
+const paddleWidth = paddle.offsetWidth;
+const gameScreenWidth = gameScreen.offsetWidth;
+
+const minPaddleX = 3;
+const maxPaddleX = gameScreenWidth - paddleWidth - 5;
+
+let animationId;
+let currentDirection = null;
+
+document.addEventListener("keydown", function (event) {
   if (event.code === "ArrowLeft") {
-    paddle.moveLeft();
+    movePaddleLeft();
   } else if (event.code === "ArrowRight") {
-    paddle.moveRight();
-  } else if (event.code === "Space") {
-    if (gameOver) {
-      restartGame();
-    } else {
-      togglePause();
-    }
+    movePaddleRight();
   }
 });
 
+document.addEventListener("keyup", function (event) {
+  if (
+    (event.code === "ArrowLeft" && currentDirection === "left") ||
+    (event.code === "ArrowRight" && currentDirection === "right")
+  ) {
+    currentDirection = null;
+    cancelAnimationFrame(animationId);
+  }
+});
+
+function movePaddleLeft() {
+  if (currentDirection !== "left") {
+    currentDirection = "left";
+    let currentPaddleX = paddle.offsetLeft;
+    if (currentPaddleX > minPaddleX) {
+      cancelAnimationFrame(animationId);
+      animationId = requestAnimationFrame(function moveLeft() {
+        if (currentDirection === "left" && currentPaddleX > minPaddleX) {
+          currentPaddleX -= 5;
+          paddle.style.left = currentPaddleX + "px";
+          // Ball follows paddle
+          if (!ballReleased) {
+            holdball();
+          }
+          animationId = requestAnimationFrame(moveLeft);
+        } else {
+          currentDirection = null;
+        }
+      });
+    }
+  }
+}
+
+function movePaddleRight() {
+  if (currentDirection !== "right") {
+    currentDirection = "right";
+    let currentPaddleX = paddle.offsetLeft;
+    if (currentPaddleX < maxPaddleX) {
+      cancelAnimationFrame(animationId);
+      animationId = requestAnimationFrame(function moveRight() {
+        if (currentDirection === "right" && currentPaddleX < maxPaddleX) {
+          currentPaddleX += 5;
+          paddle.style.left = currentPaddleX + "px";
+          // Ball follows paddle
+          if (!ballReleased) {
+            holdball();
+          }
+          animationId = requestAnimationFrame(moveRight);
+        } else {
+          currentDirection = null;
+        }
+      });
+    }
+  }
+}
+
+// Ball movement
+let ballReleased = false;
+
+// Initial position of the ball
+
+function holdball() {
+  ball.style.left = paddle.offsetLeft + paddleWidth / 2 - 10 + "px";
+  ball.style.top = paddle.offsetTop - 35 + "px";
+}
+holdball();
+
+// Start the game with spacebar
+document.addEventListener("keydown", function (event) {
+  if (event.code === "Space") {
+    if (!ballReleased) {
+      ballReleased = true;
+      moveBall();
+    }
+  }
+});
