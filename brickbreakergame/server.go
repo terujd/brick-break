b3bc26b04b5f3fdfbdb96c883051c6c1b73f746a
Vick <victor.rehnberg@gmail.com>
2023-03-09 20:04:33 +0200

    made startup text

--- brickbreakergame/server.go
+++ brickbreakergame/server.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	fmt.Println("starting")
-	static := http.FileServer(http.Dir("./falusvampen"))
+	static := http.FileServer(http.Dir("./test"))
 	http.Handle("/", static)
 
 	http.ListenAndServe(":8080", nil)
